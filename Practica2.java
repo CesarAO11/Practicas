7d564ec973b72040d6063fa68ebd8a516a62a3e2
CesarAO11 <143725010+CesarAO11@users.noreply.github.com>
2023-09-07 08:49:43 -0600

    Add files via upload

---
+++ Practica2.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author labcca
+ */
+public class Practica2 {
+    public static void main(String[] args) {
+    int base1 = 12;
+    int altura2 = 6;
+   
+    int areaRectangulo = base1 * altura2;
+    System.out.println("el area del rectangulo es: " + areaRectangulo);
+}
+}
